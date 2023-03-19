0615c489fb4a4d66b853a39ac5b405b628f670b5
Tyom8 <119136182+Tyom8@users.noreply.github.com>
2023-03-19 03:21:57 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+class Clock {
+    constructor(hour, minute, second) {
+        this.hour = hour
+        this.minute = minute
+        this.second = second
+    }
+    start() {
+        setInterval(() => {
+            this.second += 1
+            if (this.second > 59) {
+                this.minute += 1
+                this.second -= 60}
+                if (this.minute > 59) {
+                    this.hour += 1
+                    this.minute -= 60}
+                  if (this.hour > 23) {
+                        this.hour -= 24
+                    };
+                    console.clear();
+                    console.log(`${this.hour} : ${this.minute} : ${this.second}`);
+        }, 1000)
+    };
+    setAlert(_hour,_minute,_second) {
+        let timer1 = (this.hour * 60 * 60 * 1000) + (this.minute * 60 * 1000) + (this.second * 1000);
+        let timer2 = (_hour * 60 * 60 * 1000) + (_minute * 60 * 1000) + (_second * 1000);
+        let timer3;
+        if (timer2 > timer1) {
+            timer3 = timer2 - timer1;
+        } else {
+            timer3 = (24 * 60 * 60 * 1000) - (timer1 - timer2);
+        }
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve("Budelnik");
+            }, timer3)
+        })
+    }
+};
+
+let x = new Clock(21,59,55);
+x.start();
+x.setAlert(22, 00, 10).then((result) => {
+    console.log(`${result} - Football Time`);
+ });
+ x.setAlert(22, 00, 00).then((result) => {
+    console.log(`${result} - Gnal Dasi`);
+ });
+ x.setAlert(22, 00, 05).then((result) => {
+    console.log(`${result} - Durs gal`);
+ });
